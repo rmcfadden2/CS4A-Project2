a0bddd4c1292b339f4710351c2335cd60dd86fd9
Robert <mcfaddenbones@gmail.com>
2026-04-15 22:02:56 -0700

    Added BusinessAccount and CreditAccount
    Need the exception class to mark the classes as officially complete

---
+++ BusinessAccount.java
@@ -0,0 +1,78 @@
+public class BusinessAccount  extends bankAccount {
+    private String businessName;
+    private double minimumBalance;
+
+    BusinessAccount()
+    {
+        super();
+        businessName = "Business Name";
+        minimumBalance = 10000;
+    }
+    
+    BusinessAccount(double balance, String accountHolder, String accountNumber,
+                     int accountType, String businessName, double minimumBalance)
+    {
+        super(balance, accountHolder, accountNumber, accountType);
+        this.businessName = businessName;
+        this.minimumBalance = minimumBalance;
+    }
+
+    BusinessAccount(String accountHolder, String accountNumber, int accountType, String businessName, double minimumBalance)
+    {
+        super(accountHolder, accountNumber, accountType);
+        this.businessName = businessName;
+        this.minimumBalance = minimumBalance;
+    }
+
+    BusinessAccount(String accountHolder, String accountNumber, int accountType, String businessName)
+    {
+        super(accountHolder, accountNumber, accountType);
+        this.businessName = businessName;
+        this.minimumBalance = 10000;
+    }
+
+    public void setBusiness(String businessName)
+    {
+        this.businessName = businessName;
+    }
+
+    public String getBusiness()
+    {
+        return this.businessName;
+    }
+
+    public double getMinimumBalance()
+    {
+        return this.minimumBalance;
+    }
+
+    @Override
+    public void withdraw(double amount) //throws insufficientFunds;
+    {
+        try
+        {
+            double tempBal = this.getBalance();
+            tempBal -= amount;
+            if(tempBal < this.minimumBalance)
+            {
+                // need exception class
+            }
+            else
+            {
+                this.setBalance(tempBal);
+            }
+        }
+        catch()
+        {
+
+        }
+    }
+
+    @Override
+    public void deposit(double amount)
+    {
+        double tempBal = this.getBalance();
+        tempBal += amount;
+        this.setBalance(tempBal);
+    }
+}
---
+++ CreditAccount.java
@@ -0,0 +1,73 @@
+public class CreditAccount extends bankAccount{
+    private final double limit;
+    private final double withdrawFee;
+
+    CreditAccount()
+    {
+        super();
+        this.limit = 1000;
+        this.withdrawFee = 100;
+    }
+
+    CreditAccount(double balance, String accountHolder, String accountNumber, int accountType, double limit, double withdrawFee)
+    {
+        super(balance, accountHolder, accountNumber, accountType);
+        this.limit = limit;
+        this.withdrawFee = withdrawFee;
+    }
+
+    CreditAccount(String accountHolder, String accountNumber, int accountType, double limit, double withdrawFee)
+    {
+        super(accountHolder, accountNumber, accountType);
+        this.limit = limit;
+        this.withdrawFee = withdrawFee;
+    }
+
+
+    CreditAccount(String accountHolder, String accountNumber, int accountType)
+    {
+        super(accountHolder, accountNumber, accountType);
+        this.limit = 1000;
+        this.withdrawFee = 100;
+    }
+
+    public double getLimit()
+    {
+        return this.limit;
+    }
+
+    public double getWithdrawFee()
+    {
+        return this.withdrawFee;
+    }
+
+    @Override
+    public void withdraw(double amount)//throws insufficientFunds;
+    {
+        try
+        {
+            double tempBal = this.getBalance();
+            tempBal -= (amount + withdrawFee);
+            if(tempBal < (limit * -1))
+            {
+                // need exception class
+            }
+            else
+            {
+                this.setBalance(tempBal);
+            }
+        }
+        catch()
+        {
+            // need exception class
+        }
+    }
+
+    public abstract void deposit(double amount)
+    {
+        double tempBal = this.getBalance();
+        tempBal += amount;
+        this.setBalance(tempBal);
+    }
+
+}
